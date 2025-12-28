434101b1db8a70c06edc4886cd294a8c6d4762f7
Savy <savy.novachez011@proton.me>
2025-12-28 15:08:26 +0530

    preload assets for docs

---
+++ src/hooks.server.ts
@@ -0,0 +1,11 @@
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  return resolve(event, {
+    preload: ({ type }) => {
+      return (
+        type === "font" || type === "asset" || type === "js" || type === "css"
+      );
+    },
+  });
+};
